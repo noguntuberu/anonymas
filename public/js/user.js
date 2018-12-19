bf5b84d0e57dd387497102f3a8fcc781e95f4911
Nathan Oguntuberu <nathanoguntuberu@gmail.com>
2018-12-19 23:40:53 +0100

    working on javaScript logic

--- public/js/user.js
+++ public/js/user.js
@@ -3,15 +3,31 @@
 */
 
 class User {
-	constructor() {
-		this.screenName = '';
-	}
-	
-	addScreenName(name) {
-		let returnValue = false;
-		if (!empty(name))
-		{
-			
-		}
-	}
+    //
+    setScreenName(name) {
+        let returnValue = false;
+
+        if (name.length > 0) {
+            localStorage.setItem('screenName', name);
+            returnValue = true;
+        }
+    }
+    getScreenName() {
+        let name = localStorage.getItem('screenName');
+        return (name) ? name : '';
+    }
+
+    setSocketId(socketId) {
+        if(socketId) {
+            localStorage.setItem('socketId', socketId);
+        }
+    }
+    getSocketId() {
+        let id = localStorage.getItem('socketId');
+        return id ? id : '';
+    }
+
+    setOtherId(id) {
+        if
+    }
 }
