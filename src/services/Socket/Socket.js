8c02b0809943507c789857a616c17c4b9ecfa76c
noguntuberu <nateoguns.work@gmail.com>
2020-04-27 23:04:34 +0100

    modified server

--- src/services/Socket/Socket.js
+++ src/services/Socket/Socket.js
@@ -4,17 +4,12 @@
 const ConversationService = require('../Conversation/Conversation');
 
 class Socket {
-    constructor() {
-        this.socket = null;
-    }
-    initialize(main_socket) {
-        this.socket = main_socket;
-
-        this.socket.on('connection', async socket => {
+    initialize(io) {
+        io.on('connection', async socket => {
             /** */
             socket.on('join_room', room_id => {
-                this.socket.join(room_id);
-            })
+                socket.join(room_id);
+            });
 
             socket.on('start_chat', async data => {
                 const { user } = data;
@@ -25,22 +20,22 @@ class Socket {
 
                 if (!conversation || !conversation.success) return;
 
-                this.socket.emit('chat_started', conversation.payload);
+                io.emit('chat_started', conversation.payload);
             });
 
             socket.on(`typing`, data => {
                 const { room_id, is_typing } = data;
-                this.socket.to(room_id).broadcast.emit('typing', is_typing);
+                io.to(room_id).broadcast.emit('typing', is_typing);
             });
 
             socket.on('message', data => {
                 const { room_id, sender, new_message } = data;
-                this.socket.to(room_id).broadcast.emit('message', { sender, new_message });
+                io.to(room_id).broadcast.emit('message', { sender, new_message });
             });
 
             socket.on('left', data => {
                 const { room_id } = data;
-                this.socket.to(room_id).broadcast.emit('left');
+                io.to(room_id).broadcast.emit('left');
                 socket.leave(room_id);
             });
         });
